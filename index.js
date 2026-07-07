0c81c5d10ff2172a1e8748e96571585b0ad0914e
masuud254 <mascudabdi520@gmail.com>
2026-07-08 00:21:06 +0300

    Complete combineUsers function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 
+function combineUsers(...arrays) {
+  const users = [].concat(...arrays);
+  const merge_date = new Date().toDateString();
 
+  return {
+    users,
+    merge_date
+  };
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
